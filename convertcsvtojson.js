424c968452f3b054ed47be736fb8e21b47e532da
Govindarajan Subburayan <connectgovind@gmail.com>
2018-06-05 12:09:22 +0800

    Making json file

--- convertcsvtojson.js
+++ convertcsvtojson.js
@@ -3,9 +3,15 @@
 
 const csvFilePath='D:\\govind\\meanstack_workspace\\csv-to-json\\customer-data.csv'
 const csv=require('csvtojson')
+const fs = require('fs')
 const path = require('path')
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
     console.log(jsonObj);
+	fs.writeFile('customer-data.json', JSON.stringify(jsonObj, null, 2), (error)=>{
+      if (error) return process.exit(1)
+      console.log('done')
+      process.exit(0)
+    })
 })
